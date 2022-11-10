141ecaa052e555c3aa19a15aa7d7421df1650439
Muhammet Çeneli <muhammetceneli@gmail.com>
2022-11-10 14:34:43 +0300

    dockerized and deployed with kubernetes
    first

---
+++ PlatformService/Profiles/PlatformsProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Profiles{
+    public class PlatformsProfile : Profile {
+        public PlatformsProfile()
+        {
+            CreateMap<Platform, PlatformReadDto>();
+            CreateMap<PlatformCreateDto, Platform>();
+        }
+    }
+}
